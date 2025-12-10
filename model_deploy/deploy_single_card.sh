e3186ff1078edf0af775f16e7d9fd94c0e9c6e05
jt-z <jianting112358@gmail.com>
2025-12-10 15:35:36 +0800

    部署的单卡、多卡脚本，方便启动

---
+++ model_deploy/deploy_single_card.sh
@@ -0,0 +1,11 @@
+python3 -m vllm.entrypoints.openai.api_server \
+ --served-model-name autoglm-phone-9b \
+ --allowed-local-media-path /   \
+ --mm-encoder-tp-mode data \
+ --mm_processor_cache_type shm \
+ --mm_processor_kwargs "{\"max_pixels\":5000000}" \
+ --max-model-len 25480  \
+ --chat-template-content-format string \
+ --limit-mm-per-prompt "{\"image\":10}" \
+ --model zai-org/AutoGLM-Phone-9B \
+ --port 8000
---
+++ model_deploy/deploy_two_3090.sh
@@ -0,0 +1,13 @@
+python3 -m vllm.entrypoints.openai.api_server \
+  --served-model-name autoglm-phone-9b \
+  --model zai-org/AutoGLM-Phone-9B \
+  --tensor-parallel-size 2 \
+  --trust-remote-code \
+  --allowed-local-media-path / \
+  --mm-encoder-tp-mode data \
+  --mm_processor_cache_type shm \
+  --mm_processor_kwargs "{\"max_pixels\":5000000}" \
+  --max-model-len 25480 \
+  --chat-template-content-format string \
+  --limit-mm-per-prompt "{\"image\":10}" \
+  --port 8000
